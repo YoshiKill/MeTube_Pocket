b63c96b8609edb112f4f3a9a17d8654c035edf67
Oleg <46250788+YoshiKill@users.noreply.github.com>
2026-08-15 15:13:13 +0300

    Initialize settings.gradle.kts with plugin and dependency management

---
+++ settings.gradle.kts
@@ -0,0 +1,16 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+rootProject.name = "me_dial_tube"
+include(":app")
